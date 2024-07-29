9b2498e29c486a199eb96178ad372d5f5cccbf6f
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-07-29 17:22:12 -0300

    feat: introdução do curso

---
+++ Curso de react/1 - intro/hello-world/hello-world/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='App-h1'>
+          Hello, world!
+        </h1>
+        <h2 className='App-h2'>
+          test text h2
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
